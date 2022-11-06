ff48323d9cc46c69f781071f4905dc88a2c61dce
sluzerpp <bistboy@list.ru>
2022-11-06 16:18:05 +0300

    feat: complete 01-read-file task

---
+++ 01-read-file/index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require("path");
+const readableStream = fs.createReadStream(path.join(__dirname,"text.txt"));
+let data = "";
+readableStream.on("data", chunk => data += chunk)
+readableStream.on("end", () => console.log(data))
+
