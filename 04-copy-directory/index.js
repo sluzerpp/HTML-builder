bfb32a472e58c8a8b7f2bc29244893e8309b9bba
sluzerpp <bistboy@list.ru>
2022-11-06 17:06:28 +0300

    feat: complete 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,15 @@
+const { exists } = require('fs')
+const fs = require('fs/promises');
+const path = require('path');
 
+fs.mkdir(path.join(__dirname,"files-copy"),{recursive:true})
+
+fs.readdir(path.join(__dirname,"files"),{withFileTypes:true})
+.then(files => {
+    for (let file of files) {
+        console.log(path.join(__dirname,"files",file.name))
+        let fromPath = path.join(__dirname,"files",file.name),
+            toPath = path.join(__dirname,"files-copy",file.name);
+        fs.copyFile(fromPath,toPath)
+    }
+})
